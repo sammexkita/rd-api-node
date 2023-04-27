a3711a945b7639e0c508ac62247cd96651959733
kita-igniter <smtecsystem.469@gmail.com>
2023-04-27 13:50:22 -0300

    fix: #01 authentication

--- src/middlewares/rdstation/check-expires-date-token.ts
+++ src/middlewares/rdstation/check-expires-date-token.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { knex } from "../../database";
 import { RevalidateAccessToken } from "../../utils/rdstation/revalidate-access-token";
 import { isBefore } from "date-fns";
@@ -5,8 +6,20 @@ import { isBefore } from "date-fns";
 export async function checkExpiresDateToken() {
   const { expires_at } = await knex('rdstation').select('expires_at').first();
   let isExpired = isBefore(expires_at, new Date());
-  
+
   if (isExpired) { 
-    await RevalidateAccessToken();
+    return await RevalidateAccessToken();
+  }
+
+  const { token } = await knex('rdstation').select('token').first();
+  
+  const data = await axios.get('https://api.rd.services/platform/contacts/email:leadtest@email.com', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+  }).then(res => res.data).catch(err => err.response.data.error);
+
+  if (data === "invalid_token") {
+    return await RevalidateAccessToken();
   }
 }
